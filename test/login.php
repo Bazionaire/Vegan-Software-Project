e71b21c4237e8effcad6914c368af9fdbfaccd78
Bazionaire <Baz@live.nl>
2020-02-19 21:12:11 +0000

    php Login

---
+++ test/login.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+
+// Includes
+require_once "./Includes/Database/db_connect.php";
+include "./Includes/Database/userQueries.php";
+include "./Includes/Utilities/AdminSetup.php";  // DEVELOPMENT ONLY
+
+$userFeedback = null;
+
+// Entry should only be from login form via POST request
+if (IsSet($_POST) && IsSet($_POST["uname"]) && IsSet($_POST["psw"])) {
+
+    // POST request and user has entered data in both form fields
+    $username = trim($_POST['uname']);
+    $password = trim($_POST['psw']);
+
+    // Data in both fields so check login credentials
+    $userData = checkUserLogin($db, $username, $password);
+    if ($userData != null) {
+
+        // Valid login credentials so set session variable and redirect
+        $_SESSION["user"] = $username;
+        $db->close();
+        header("Location: ./index.php");
+        exit();
+    } else {
+
+        // Invalid login credentials so inform user
+        $userFeedback = "Username and password did not match.";
+    }
+}
+
+// Incomplete or no data in form fields
+session_destroy();
+$db->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Vegan App Login  </title>
+    <link rel="stylesheet" type="text/css" href="Style.css" >
+</head>
+<body class="login_page">
+<div class="loginbox">
+    <img src="images/computer-1331579_1280.png" class="profile">
+    <h1> Login Here</h1>
+    <form>
+        <p>Username</p>
+        <input type="text" name="" placeholder="Enter Username">
+        <p> Password</p>
+        <input type="password" name="" placeholder="Enter Password">
+        <input type="submit" name="" value="Login"><br>
+        <a href="#">Forgot your passwork</a> <br>
+        <a href="RegistrationForm.html">Don't have an account </a>
+
+    </form>
+
+</div>
+</body>
+</html>
