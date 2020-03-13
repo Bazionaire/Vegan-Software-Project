81c5ed7c02462c5ff5792435cda0b00cf7acac3e
1914618 <dennisabhay@gmail.com>
2020-03-13 11:39:21 +0000

    AboutUs

---
+++ AboutUs/about.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Intranet Assignment</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<header>
+   <?php include("Header.php"); ?>
+</header>
+
+
+<body>
+
+<div class="about-section">
+    <h1 style="text-align:center">About Us Page</h1>
+    <p>Hello, Welcome to RGU VEGAN PRODUCT website. So happy that you have decided to visit us today!
+        We are a group of six members Precious, Baz, Stef, Micheal, Helen and Aminu. We've been working
+        on this webpage for quite sometime now, the website was designed to search for vegan products along side
+        vegan recipes online and the search would be retrieved on a landing page. We have over hundreds of vegan recipe
+        for breakfast, Lunch and dinner as well as delicious vegans deserts
+
+        <br>The team came up with this idea during school coursework task at Robert Gordon University Aberdeen, and with the
+        help of Dr Kit Ying Hui Our course work Coordinator, we were able to break through all stumbling blocks that came our way.</br>
+
+        <b>Find what you love and you can order it Online.</b>
+
+
+        <b>We Like to keep things simple. But you probably already figured that out.
+            Thank you for stopping by.</b></p>
+
+</div>
+
+<h2 style="text-align:center">Our Team</h2>
+<div class="row" >
+    <div class="column">
+        <div class="card">
+            <img src="Image/use.jpg" alt="Precious"  style="width: 50%">
+            <div class="container">
+                <h2>Precious</h2>
+                <p>Group Member</p>
+                <p>dennisabhay@gmail.com</p>
+                <p><button class="button">Contact</button></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row" style="border-radius: 50%">
+        <div class="column">
+            <div class="card" align="center" style="border-radius: 10%">
+                <img src="Image/use.jpg" alt="Baz"  style="width: 50%; border-radius: 50%">
+                <div class="container" style="border-radius: 50%">
+                    <h2>Baz</h2>
+                    <p>Group Member</p>
+                    <p>baz@gmail.com</p>
+                    <a href="mailto:baz@gmail.com"><p><button class="button" >Contact</button></p></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="Image/use.jpg" alt="Stef"  style="width: 50%">
+                    <div class="container">
+                        <h2>Stef</h2>
+                        <p>Group Member</p>
+                        <p>stef@gmail.com</p>
+                        <p><button class="button">Contact</button></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="column">
+                    <div class="card">
+                        <img src="Image/use.jpg" alt="Micheal"  style="width: 50%">
+                        <div class="container">
+                            <h2>Micheal</h2>
+                            <p>Group Member</p>
+                            <p>micheal@gmail.com</p>
+                            <p><button class="button">Contact</button></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="column">
+                        <div class="card">
+                            <img src="Image/use.jpg" alt="Helen"  style="width: 50%">
+                            <div class="container">
+                                <h2>Helen</h2>
+                                <p>Group Member</p>
+                                <p>helen@gmail.com</p>
+                                <p><button class="button">Contact</button></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                                <img src="Image/use.jpg" alt="Aminu"  style="width: 50%">
+                                <div class="container">
+                                    <h2>Aminu</h2>
+                                    <p>Group Member</p>
+                                    <p>aminu@gmail.com</p>
+                                    <p><button class="button">Contact</button></p>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                        <main>
+
+                        </main>
+
+
+                        <footer>
+
+
+                        </footer>
+</body>
